60dcb0e88431e49a61be8674ff1fe6962670513c
avirajj98 <31987973+avirajj98@users.noreply.github.com>
2017-09-15 20:10:01 +0530

    asfasfasf

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -62,21 +62,6 @@ public class StudentGroup implements StudentArrayOperation {
 	public void setStudent(Student student, int index) {
 		// Add your implementation here
 
-		if(student==null || (index <0 || index > this.students.length)){
-
-			throw new IllegalArgumentException("invalid");
-		}
-		else{
-
-			for(int i=0;i<this.students.length;i++){
-
-				if( index ==i){
-
-					this.students[i]=student;
-				}
-			}
-
-		}
 	}
 
 	@Override
